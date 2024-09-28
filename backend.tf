18a5fdc65ec99c9bfe5b838ab945f3f83edc963c
shashank753 <shashanksiddappa@gmail.com>
2024-09-28 10:15:27 +0530

    Delete backend.tfcc

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "shashank-s3-demo-abc" 
-    key            = "terraform-simple/modules/ec2/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
