8ee9315a3cf1a3c1ecca654bc49bfb87f0f6cde0
shashank753 <shashanksiddappa@gmail.com>
2025-07-14 15:07:38 +0530

    Update main.tf
    changeing the bucket name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "shashank-s3-demo-abc-1" 
+  bucket = "shashank-s3-demo-abc-1-1" 
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
